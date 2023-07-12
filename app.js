c93412b39f0da9dc113f9b10009eafba51261e31
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-07-12 23:33:28 +0700

    feat: add helmet for more security

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
