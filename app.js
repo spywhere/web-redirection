cba5f655de485cdcb66c3a3a088a69946fb30988
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-07-13 00:08:39 +0700

    refactor: use koa instead of express

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-const express = require('express');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
+import Koa from 'koa';
+import helmet from 'koa-helmet';
+import compress from 'koa-compress';
+import cookie from 'koa-cookie';
+import Router from '@koa/router';
 
 const port = process.env.PORT || 3000;
-const app = express();
+const app = new Koa();
 
 app.use(helmet());
-app.use(cookieParser());
+app.use(compress());
+app.use(cookie.default());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const router = new Router();
+
+router.get('/', async (ctx) => {
+  ctx.body = 'hello world';
 });
 
-app.get('/:keyword', (req, res) => {
-  const baseUrl = `${req.protocol}://${req.get('host')}/`;
-  const url = req.cookies[req.params.keyword] || baseUrl;
+router.get('/:keyword', async (ctx) => {
+  const cookies = ctx.cookie || {};
+  const url = cookies[ctx.params.keyword] || '/';
 
-  console.log(JSON.stringify(req.cookies, undefined, 2));
+  console.log(JSON.stringify(cookies, undefined, 2));
 
-  res.redirect(url);
+  ctx.redirect(url);
+  ctx.body = '';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.use(async (context) => {
+  context.body = '';
+  context.status = 404;
 });
 
 app.listen(port, () => {
